ec9abf2589d0151fb64f0a3e25ce2b3f3d893e22
Sidharth Manikuttan <115486379+Sidharth-Manikuttan@users.noreply.github.com>
2025-01-22 11:26:28 +0530

    Time: 693 ms (82.12%), Space: 84.9 MB (53.88%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -0,0 +1,22 @@
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        ROWS,COLS=len(isWater),len(isWater[0])
+        q=deque()
+        res=[[-1]*COLS for _ in range(ROWS)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                if isWater[r][c]:
+                    q.append((r,c))
+                    res[r][c]=0
+        while q:
+            r,c=q.popleft()
+            h=res[r][c]
+            neighbors=[[r+1,c],[r,c+1],[r-1,c],[r,c-1]]
+            for nr,nc in neighbors:
+                if (nr<0 or nc<0 or nr==ROWS or nc==COLS or res[nr][nc]!=-1):
+                    continue
+                q.append((nr,nc))
+                res[nr][nc]=h+1
+        return res
+
+        
